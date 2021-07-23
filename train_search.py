0ca8e8510794bf52764acc67220fcc2a9c42ede3
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-24 00:48:46 +0530

    bs=200

--- train_search.py
+++ train_search.py
@@ -25,7 +25,7 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 parser = argparse.ArgumentParser("cifar")
 parser.add_argument('--data', type=str, default='./data', help='location of the data corpus')
 parser.add_argument('--set', type=str, default='cifar10', help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=150, help='batch size')
+parser.add_argument('--batch_size', type=int, default=200, help='batch size')
 parser.add_argument('--learning_rate', type=float, default=0.1, help='init learning rate')
 parser.add_argument('--learning_rate_min', type=float, default=0.0, help='min learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
