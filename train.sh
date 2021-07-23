22eb563d8b9672764d7781e6f1e64f50f82caf92
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-23 23:53:08 +0530

    train again

--- train.sh
+++ train.sh
@@ -6,6 +6,6 @@ pwd
 source activate /abhibha-volume/darts
 conda info
 which python
-#python /abhibha-volume/PC-DARTS/train.py
+python /abhibha-volume/PC-DARTS/train.py  --auxiliary  --cutout 
 # python train_search.py --unrolled
-python test.py 
+# python test.py 
