72532c215f374d551c93a4f6604e82c86cd72a3c
Abhibha Gupta <abhibha1807@gmail.com>
2021-08-01 13:27:39 +0530

    valid_transform

--- train.py
+++ train.py
@@ -107,9 +107,9 @@ def main():
   'train':
   datasets.ImageFolder(root=traindir, transform=train_transform),
   'val':
-  datasets.ImageFolder(root=validdir, transform=train_transform),
+  datasets.ImageFolder(root=validdir, transform=valid_transform),
   'test':
-  datasets.ImageFolder(root=testdir, transform=train_transform)
+  datasets.ImageFolder(root=testdir, transform=valid_transform)
 }
 
   train_data=data['train']
